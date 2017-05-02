94e8aed75a094867e0c67dbaa3a8e1e56dab9e75
Zeokat <zeokat@gmail.com>
2017-05-02 12:34:21 +0200

    Update FormatFollowAutolink.php

--- src/Listener/FormatFollowAutolink.php
+++ src/Listener/FormatFollowAutolink.php
@@ -7,40 +7,71 @@
 namespace Zeokat\FollowAutolink\Listener;
 
 use Flarum\Event\ConfigureFormatter;
-use Flarum\Settings\SettingsRepositoryInterface;
+use Flarum\Event\ConfigureFormatterParser;
+use Flarum\Event\ConfigureFormatterRenderer;
 use Illuminate\Contracts\Events\Dispatcher;
 
 class FormatFollowAutolink
 {
     /**
-     * @var SettingsRepositoryInterface
-     */
-    protected $settings;
-
-    /**
-     * @param SettingsRepositoryInterface $settings
-     */
-    public function __construct(SettingsRepositoryInterface $settings)
-    {
-        $this->settings = $settings;
-    }
-
-    /**
      * @param Dispatcher $events
      */
     public function subscribe(Dispatcher $events)
     {
-        $events->listen(ConfigureFormatter::class, [$this, 'addFollowAutolinkFormatter']);
+        $events->listen(ConfigureFormatter::class, [$this, 'configure']);
+        $events->listen(ConfigureFormatterParser::class, [$this, 'parse']);
+        $events->listen(ConfigureFormatterRenderer::class, [$this, 'render']);
+    }
+
+     /**
+     * @param ConfigureFormatter $event
+     */
+    public function configure(ConfigureFormatter $event)
+    {
+        $configurator = $event->configurator;
+
+        $tagName = 'USERMENTION';
+
+        $tag = $configurator->tags->add($tagName);
+        $tag->attributes->add('username');
+        $tag->attributes->add('id')->filterChain->append('#uint');
+        $tag->attributes['id']->required = false;
+
+        $tag->template = '<a href="{$PROFILE_URL}{@username}" class="UserMention">@<xsl:value-of select="@username"/></a>';
+        $tag->filterChain->prepend([static::class, 'addId'])
+            ->addParameterByName('userRepository')
+            ->setJS('function() { return true; }');
+
+        $configurator->Preg->match('/\B@(?<username>[a-z0-9_-]+)(?!#)/i', $tagName);
     }
 
     /**
-     * @param ConfigureFormatter $event
+     * @param ConfigureFormatterParser $event
      */
-    public function addFollowAutolinkFormatter(ConfigureFormatter $event)
+    public function parse(ConfigureFormatterParser $event)
     {
-        $event->configurator->plugins->set(
-            'FollowInternalAutolink',
-            "Zeokat\\FollowAutolink\\TextFormatter\\Plugins\\FollowInternalAutolink\\Configurator"
-        );
+        $event->parser->registeredVars['userRepository'] = $this->users;
+    }
+
+    /**
+     * @param ConfigureFormatterRenderer $event
+     */
+    public function render(ConfigureFormatterRenderer $event)
+    {
+        $event->renderer->setParameter('PROFILE_URL', $this->url->toRoute('user', ['username' => '']));
+    }
+
+    /**
+     * @param $tag
+     * @param UserRepository $users
+     * @return bool
+     */
+    public static function addId($tag, UserRepository $users)
+    {
+        if ($id = $users->getIdForUsername($tag->getAttribute('username'))) {
+            $tag->setAttribute('id', $id);
+
+            return true;
+        }
     }
 }
