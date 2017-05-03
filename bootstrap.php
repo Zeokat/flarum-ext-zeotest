d4a0ffb082a584b8d78790b78cafd484b4fe40fb
Zeokat <zeokat@gmail.com>
2017-05-04 00:13:31 +0200

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -8,5 +8,5 @@ namespace Zeokat\FollowAutolink;
 use Illuminate\Contracts\Events\Dispatcher;
 
 return function (Dispatcher $events) {
-    $events->subscribe(Listeners\AddBBCode::class);
+    $events->subscribe(Listener\AddBBCode::class);
 };
