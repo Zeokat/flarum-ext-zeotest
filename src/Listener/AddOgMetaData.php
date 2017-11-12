e0bf9089662b9f5533ff88b37de729a04bedc4e5
Zeokat <zeokat@gmail.com>
2017-11-13 00:47:40 +0100

    Update AddOgMetaData.php

--- src/Listener/AddOgMetaData.php
+++ src/Listener/AddOgMetaData.php
@@ -6,26 +6,28 @@
 
 namespace Zeokat\AddOgData\Listener;
 
-use Flarum\Event\ConfigureFormatter;
+use Flarum\Event\ConfigureClientView;
 use Illuminate\Contracts\Events\Dispatcher;
 
 class AddOgMetaData
 {
+    /**
+     * @param Dispatcher $events
+     */
     public function subscribe(Dispatcher $events)
     {
-        $events->listen(ConfigureFormatter::class, [$this, 'addBBCode']);
+        $events->listen(ConfigureClientView::class, [$this, 'addAssets']);
     }
     
-    public function addBBCode(ConfigureFormatter $event)
+    public function addAssets(ConfigureClientView $event)
     {
-        $event->configurator->BBCodes->addCustom(
-            '[INTURL={URL}]{TEXT}[/INTURL]',
-            '<a href="{URL}">{TEXT}</a>'
-        );
-        
-        $event->configurator->BBCodes->addCustom(
-            '[INTURLB={URL}]{TEXT}[/INTURLB]',
-            '<a href="{URL}" target="_blank" rel="noopener noreferrer">{TEXT}</a>'
-        );
+        if ($event->isForum()) {
+            // Add google analytics if tracking UA has been configured.
+            if ($this->settings->get('flagrow.analytics.statusGoogle') && $this->settings->get('flagrow.analytics.googleTrackingCode')) {
+                $rawJs = file_get_contents(realpath(__DIR__ . '/../../assets/js/google-analytics.js'));
+                $js    = str_replace("%%TRACKING_CODE%%", $this->settings->get('flagrow.analytics.googleTrackingCode'), $rawJs);
+                $event->view->addHeadString($js);
+            }
+        }
     }
 }
