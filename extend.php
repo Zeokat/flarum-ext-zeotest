933a600b06f3899ecff47f055d84f5e24fd31c51
Zeokat <zeokat@gmail.com>
2019-09-12 02:17:00 +0200

    Update extend.php

--- extend.php
+++ extend.php
@@ -14,6 +14,7 @@ return [
   (new Extend\Frontend('forum'))
     ->content(function (Document $document) {
       $ads_script = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>';
+      $ads_script .= '<script>(adsbygoogle = window.adsbygoogle || []).push({google_ad_client: "ca-pub-1224297555351654",enable_page_level_ads: true});</script>';
       $document->head[] = $ads_script;
       })
 ];
